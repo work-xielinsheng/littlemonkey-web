46ffce93397e793da1b5ce4cf084c9d29061f866
minmx <Zhangmengqin2018>
2018-06-16 09:16:48 +0800

    增加權限管理

--- src/main/java/com/littlemonkey/web/test/TestService.java
+++ src/main/java/com/littlemonkey/web/test/TestService.java
@@ -1,9 +1,8 @@
 package com.littlemonkey.web.test;
 
 import com.littlemonkey.web.annotation.Resources;
-import com.littlemonkey.web.authority.WebUsernamePasswordToken;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 
 /**
@@ -16,7 +15,7 @@ public class TestService {
 
     public void get(long id) {
         Subject subject = SecurityUtils.getSubject();
-        subject.login(new WebUsernamePasswordToken("xielinsheng","123456"));
+        subject.login(new UsernamePasswordToken("xielinsheng", "123456"));
         subject.isPermitted("????");
     }
 }
